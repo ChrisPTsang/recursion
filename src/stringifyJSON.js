54467b8d45a777bd756246f7991df5fc0f4c54ad
Chris Tsang <chris.p.tsang@gmail.com>
2015-06-07 16:52:47 -0700

    Passed all tests for stringifyJSON.
    Not entirely sure if we are supposed to use functions like .map to achieve the goal for arrays.  Could really use _.map for the objects as well if I include the library?

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,32 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === null) {
+    return 'null';
+  }
+  if (obj === undefined || typeof(obj) === 'function') {
+    return null;
+  }
+  if (typeof(obj) === 'boolean' || typeof(obj) === 'number') {
+    return obj.toString();
+  }
+  if (typeof(obj) === 'string') {
+    return '"' + obj + '"';
+  }
+  if (Array.isArray(obj)) {
+    return '[' +
+      obj.map(function(value) {
+        return stringifyJSON(value);
+      }).join(",") + ']';
+  }
+  if (typeof(obj) === 'object') {
+    var result = [];
+    for (var key in obj) {
+      var value = stringifyJSON(obj[key]);
+      if (value !== null) {
+        result.push('"' + key + '"' + ":" + value);
+      }
+    }
+    return "{" + result.join(",") + "}";
+  }
 };
