fa930227aac0d3ec52ff581469306a796407279d
Chris Tsang <chris.p.tsang@gmail.com>
2015-06-10 00:56:11 -0700

    Completed and passed all tests for getElementsByClassName().  Created a separate traverse() function for traversing the DOM which is called recursively until no more child nodes are found.  Not sure if special if statement to examine the body element the correct way to approach this.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,30 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className) {
   // your code here
+  var result = [],
+    element = document.body;
+
+  if (element.classList.contains(className)) {
+    result.push(element);
+  }
+
+  traverse(element);
+
+  function traverse(node) {
+    var children = node.children;
+
+    for (var i = 0; i < children.length; i++) {
+      if (children[i].hasChildNodes()) {
+        traverse(children[i]);
+      }
+      if (children[i].classList.contains(className)) {
+        result.push(children[i]);
+      }
+    }
+  }
+
+  return result;
+
 };
